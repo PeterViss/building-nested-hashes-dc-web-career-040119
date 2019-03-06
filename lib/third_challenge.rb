e014c17b3da582d97ea0f20e4f2db2fe9c00871a
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-06 01:34:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,10 +14,8 @@ def third_challenge
         age: "15"
         status: "alive"#your key/value pairs here
         },
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried}"
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-        ]
+      :hero_friends => []
+        
    },
    :capulet => {
       :patriarch => {
@@ -33,10 +31,8 @@ def third_challenge
         age: "15"
         status: "alive"#your key/value pairs here
         },
-      :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
-      ]
+      :heroine_friends => []
+      
    }
   }
 
